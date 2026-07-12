2d88b94c24ecae8323bd2b6366901476f07f09f3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "graph_services_accounts" {
-  description = "All graph_services_account resources"
-  value       = azurerm_graph_services_account.graph_services_accounts
-}
 output "graph_services_accounts_application_id" {
-  description = "List of application_id values across all graph_services_accounts"
-  value       = [for k, v in azurerm_graph_services_account.graph_services_accounts : v.application_id]
+  description = "Map of application_id values across all graph_services_accounts, keyed the same as var.graph_services_accounts"
+  value       = { for k, v in azurerm_graph_services_account.graph_services_accounts : k => v.application_id }
 }
 output "graph_services_accounts_billing_plan_id" {
-  description = "List of billing_plan_id values across all graph_services_accounts"
-  value       = [for k, v in azurerm_graph_services_account.graph_services_accounts : v.billing_plan_id]
+  description = "Map of billing_plan_id values across all graph_services_accounts, keyed the same as var.graph_services_accounts"
+  value       = { for k, v in azurerm_graph_services_account.graph_services_accounts : k => v.billing_plan_id }
 }
 output "graph_services_accounts_name" {
-  description = "List of name values across all graph_services_accounts"
-  value       = [for k, v in azurerm_graph_services_account.graph_services_accounts : v.name]
+  description = "Map of name values across all graph_services_accounts, keyed the same as var.graph_services_accounts"
+  value       = { for k, v in azurerm_graph_services_account.graph_services_accounts : k => v.name }
 }
 output "graph_services_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all graph_services_accounts"
-  value       = [for k, v in azurerm_graph_services_account.graph_services_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all graph_services_accounts, keyed the same as var.graph_services_accounts"
+  value       = { for k, v in azurerm_graph_services_account.graph_services_accounts : k => v.resource_group_name }
 }
 output "graph_services_accounts_tags" {
-  description = "List of tags values across all graph_services_accounts"
-  value       = [for k, v in azurerm_graph_services_account.graph_services_accounts : v.tags]
+  description = "Map of tags values across all graph_services_accounts, keyed the same as var.graph_services_accounts"
+  value       = { for k, v in azurerm_graph_services_account.graph_services_accounts : k => v.tags }
 }
 
